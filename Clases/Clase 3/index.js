e32cc716e90592e5daf9f8aed2493dae6e8f5a7d
Luis Cascante <luisalonzocascante@gmail.com>
2021-04-13 19:04:26 -0600

    crear elemento

--- Clases/Clase 3/index.js
+++ Clases/Clase 3/index.js
@@ -1,11 +1,24 @@
-function addElement() {
-  // crea un nuevo div
-  // y añade contenido
-  var newDiv = document.createElement("div");
-  var newContent = document.createTextNode("Hola!¿Qué tal?");
-  newDiv.appendChild(newContent); //añade texto al div creado.
-
-  // añade el elemento creado y su contenido al DOM
-  var currentDiv = document.getElementById("div1");
-  document.body.insertBefore(newDiv, currentDiv);
+function crearElemento() {
+  // Crear nuevo elemento, pero no lo agrega
+  const nuevoParrafo = document.createElement("p");
+  const nuevoContenidoParrafo = document.createTextNode("Texto de prueba");
+  console.log("Etiqueta p", nuevoParrafo.outerHTML);
+  console.log("Contenido de p", nuevoContenidoParrafo);
+  nuevoParrafo.appendChild(nuevoContenidoParrafo);
+  console.log("Después de append", nuevoParrafo.outerHTML);
+  const elementoRefencia = document.getElementById("texto-2");
+  document.body.insertBefore(nuevoParrafo, elementoRefencia);
 }
+
+const numero = 0;
+const string = "Esto es una cadena de caracteres";
+const booleano = true;
+
+// not        false
+if (!(numero && booleano)) {
+  // console.log("Hola");
+}
+
+// && AND => Y
+// || OR => O
+// ! NOT negación
