6e88ab5b379a15d265cfab321dbb380eed481068
Luis Cascante <luisalonzocascante@gmail.com>
2021-04-13 18:12:08 -0600

    clase 2 empezando

--- Clases/Clase 2/index.js
+++ Clases/Clase 2/index.js
@@ -1,17 +1,22 @@
 function foo() {
   document.getElementById("demo").innerHTML += " Hola Mundo!";
-  const imagen = document.getElementById("imagen");
-  imagen.src = "../assets/accessible-icon-brands.svg";
+  let imagen = document.getElementById("imagen");
+  console.log(imagen.outerHTML);
+  imagen.src = "../../assets/accessible-icon-brands.svg";
+  console.log(imagen.outerHTML);
 
   setTimeout(() => {
     imagen.src =
       "https://i1.wp.com/elanillounico.com/wp-content/uploads/2015/03/JRR-Tolkien15.jpg?fit=760%2C1076&ssl=1";
+    console.log(imagen.outerHTML);
   }, 5000);
 }
 
 function suma(sumando1, sumando2) {
   return sumando1 + sumando2;
 }
+
+suma(2, 3);
 
 const documento = {
   suma,
