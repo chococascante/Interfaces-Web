aa532c639e072dd1042e354c4dc08ef6e1dcd66e
Luis Cascante <luisalonzocascante@gmail.com>
2021-04-20 21:56:41 -0600

    clase 4

---
+++ Clases/Clase 4/index.js
@@ -0,0 +1,10 @@
+function manejarBarraNavegacion() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("movil");
+
+  // if (navbar.className === "navbar") {
+  //   navbar.className += " movil";
+  // } else {
+  //   navbar.className = "navbar";
+  // }
+}
