7a840fabffae2bf7006fdf8284690a7c1474d7ee
Luis Cascante <luisalonzocascante@gmail.com>
2021-04-30 16:24:41 -0600

    clase 7

---
+++ Clases/Clase 7/index.js
@@ -0,0 +1,9 @@
+function mostrarOcultarMenu() {
+  // const boton = document.getElementById("boton-menu"); // Elemento
+  // const clases = boton.classList; // Lista de clases
+  // clases.toggle("header--nav--dropdown");
+
+  document
+    .getElementById("header-nav-menu")
+    .classList.toggle("header--nav--dropdown");
+}
