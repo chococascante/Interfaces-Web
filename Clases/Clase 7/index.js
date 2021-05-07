c38b67600376bcb615ea7596e44be57b000bfc36
Luis Cascante <luisalonzocascante@gmail.com>
2021-05-07 09:51:01 -0600

    clase 7

--- Clases/Clase 7/index.js
+++ Clases/Clase 7/index.js
@@ -7,3 +7,41 @@ function mostrarOcultarMenu() {
     .getElementById("header-nav-menu")
     .classList.toggle("header--nav--dropdown");
 }
+
+let indice = 1;
+
+function cambiarSlide(n) {
+  const slides = document.getElementsByClassName("slide");
+
+  if (n > slides.length) {
+    n = 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[n - 1].style.display = "block";
+}
+
+function slideAnterior() {
+  indice = indice - 1;
+  cambiarSlide(indice);
+}
+
+function siguienteSlide() {
+  indice = indice + 1;
+  cambiarSlide(indice);
+}
+
+function cambiarIndice(n) {
+  cambiarSlide((indice += n));
+}
+
+function autoplay() {
+  setInterval(() => {
+    cambiarSlide((indice += 1));
+  }, 1000);
+}
+
+window.onload = autoplay;
